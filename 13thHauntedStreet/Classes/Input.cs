79ccf51ff60e2ff5877b98cf370ca38416c77024
Marco Rodrigues <marco.rdrgs2@eduge.ch>
2022-02-24 15:50:01 +0100

    first commit

---
+++ 13thHauntedStreet/Classes/Input.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace _13thHauntedStreet
+{
+    class Input
+    {
+        public Keys Up;
+        public Keys Down;
+        public Keys Left;
+        public Keys Right;
+    }
+}
---
+++ 13thHauntedStreet/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _13thHauntedStreet
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
